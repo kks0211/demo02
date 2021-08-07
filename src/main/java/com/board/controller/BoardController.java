0dafc7c9c2ac8cfee3de1493b852f9811d06e8c9
kks <kks_0211@naver.com>
2021-08-07 12:55:19 +0900

    20210807

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -5,14 +5,13 @@ import com.board.domain.Criteria;
 import com.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Log4j
 @Controller
@@ -72,9 +71,10 @@ public class BoardController {
     }*/
 
     @GetMapping("/list")
-    public void list(Criteria criteria, Model model) {
+    public String list(Criteria criteria, Model model) {
         log.info("list");
         model.addAttribute("list", service.getList(criteria));
+        return "/board/list";
     }
 
     @GetMapping("/register")
